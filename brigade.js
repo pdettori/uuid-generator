6e736cb808d6b361a6f1ae71e12470c7407edafe
Paolo Dettori <dettori@us.ibm.com>
2018-04-24 22:00:45 -0400

    pippo

--- brigade.js
+++ brigade.js
@@ -1,5 +1,5 @@
 const { events } = require("brigadier");
 
 events.on("push", function(e, project) {
-  console.log("Hi there Brigade !! received push for commit " + e.commit)
+  console.log("Hi there Brigade !! received push for commit " + e + " project: " + project)
 })
